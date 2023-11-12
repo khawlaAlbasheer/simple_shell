f3dcc3f2fc20da4b0af64ae74d805a82d489a475
Abubeker Abdullahi <abukiabdullahi1@gmail.com>
2023-11-12 11:23:00 +0300

    new change

--- shell.h
+++ shell.h
@@ -52,7 +52,7 @@ typedef struct liststr
 /**
  *struct pass_info - contains pseudo-arguments to pass into a function,
  *		allowing uniform prototype for function pointer struct
- *@arg: a string generated from getline containing arguements
+ *@arg: a string generated from getline containing arguments
  *@argv: an array of strings generated from arg
  *@path: a string path for the current command
  *@argc: the argument count
